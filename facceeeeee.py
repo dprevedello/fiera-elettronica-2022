283b925dcaff2bcd42361a83bb4d10a227e37a62
ciroooooo <cirillo773@gmail.com>
2022-03-01 13:29:47 +0100

    integrazione face detection
    integrazione face detection

--- facceeeeee.py
+++ facceeeeee.py
@@ -13,45 +13,49 @@ mpDraw = mp.solutions.drawing_utils
 faceDetection = mpFaceDetection.FaceDetection(0.75)
 detector = FaceMeshDetector(maxFaces=1)
 
-    
-while True:
-    
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = faceDetection.process(imgRGB)
-    print(results)
-    img, faces = detector.findFaceMesh(img, draw=False)
-    if faces:
-        face = faces[0]
-        pointLeft = face[145]
-        pointRight = face[374]
-        w, _ = detector.findDistance(pointLeft, pointRight)
-        W = 6.3
-        f = 840
-        d = (W * f) / w
-        print(d)
-        cvzone.putTextRect(img, f'Distanza: {int(d)}cm',
-                           (face[10][0] - 100, face[10][1] - 50),
-                           scale=2)
+class faccia():   
+    def facce():
+        while True:
+            
+            success, img = cap.read()
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = faceDetection.process(imgRGB)
+           
+            #print(results)
+            img, faces = detector.findFaceMesh(img, draw=False)
+            if faces:
+                face = faces[0]
+                pointLeft = face[145]
+                pointRight = face[374]
+                w, _ = detector.findDistance(pointLeft, pointRight)
+                W = 6.3
+                f = 840
+                d = (W * f) / w
+                d=d-6
+                print(d)
+                cvzone.putTextRect(img, f'Distanza: {int(d)}cm',
+                                   (face[10][0] - 100, face[10][1] - 50),
+                                   scale=2)
+                
+            if results.detections:
+                for id, detection in enumerate(results.detections):
+                    # mpDraw.draw_detection(img, detection)
+                    # print(id, detection)
+                    # print(detection.score)
+                    # print(detection.location_data.relative_bounding_box)
+                    bboxC = detection.location_data.relative_bounding_box
+                    ih, iw, ic = img.shape
+                    bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                           int(bboxC.width * iw), int(bboxC.height * ih)
+                    cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            key = cv2.waitKey(1)
+            if key == 27 or key == 113:
+                cv2.destroyAllWindows()
+                break
+                
+            
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
+
         
-    if results.detections:
-        for id, detection in enumerate(results.detections):
-            # mpDraw.draw_detection(img, detection)
-            # print(id, detection)
-            # print(detection.score)
-            # print(detection.location_data.relative_bounding_box)
-            bboxC = detection.location_data.relative_bounding_box
-            ih, iw, ic = img.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
-                   int(bboxC.width * iw), int(bboxC.height * ih)
-            cv2.rectangle(img, bbox, (255, 0, 255), 2)
-    key = cv2.waitKey(1)
-    if key == 27 or key == 113:
-        cv2.destroyAllWindows()
-        break
-        
-    
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-    
 
--- progetto/gui.py
+++ progetto/gui.py
@@ -3,15 +3,40 @@ import PySimpleGUI as sg
 import cv2
 import numpy as np
 import cvzone
-from cvzone.HandTrackingModule import HandDetector
 import math
+import mediapipe as mp
+import time
+from cvzone.FaceMeshModule import FaceMeshDetector
+
+
+detector = FaceMeshDetector(maxFaces=1)
 
 def temperatura():
     return 23
 
 def face_detection(frame):
-#faccia trovata, frame con contorno della faccia, faccia, distanza
-    return False,frame, frame, 12 
+    img, faces = detector.findFaceMesh(frame, draw=False)
+    if faces:
+        print (faces)
+        print(min(faces[0,:,0]), max(faces[0,:,0]))
+        face = faces[0]
+        pointLeft = face[145]
+        pointRight = face[374]
+        w, _ = detector.findDistance(pointLeft, pointRight)
+        W = 6.3
+        f = 840
+        d = (W * f) / w
+        d=d-6
+        
+        cvzone.putTextRect(img, f'Distanza: {int(d)}cm',
+                           (face[10][0] - 100, face[10][1] - 50),
+                           scale=2)
+                                    
+        #cv2.rectangle(img, bbox, (255, 0, 255), 2)
+        return True, img, img, d
+        
+    #faccia trovata, frame con contorno della faccia, faccia, distanza
+    return False, frame, frame, None 
 
 def mask_detection(frame):
     return False
@@ -22,6 +47,9 @@ layout = [[sg.Image(filename='', key='webcam')]]
 window = sg.Window("GUI - progetto FieraElettronica",resizable = False, grab_anywhere = True, margins = (0, 0), icon='images/logoponti.ico').Layout(layout)
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+pTime = 0
     
 while True:   #event loop
     event, values = window.read(timeout=10)
@@ -29,7 +57,9 @@ while True:   #event loop
         break
     ret, frame = cap.read()
     frame = cv2.flip(frame,1)
+    detected, frame, face, distanza=face_detection(frame)
     imgbytes = cv2.imencode('.png', frame)[1].tobytes()  
     window['webcam'].update(data=imgbytes)
+       
 
 window.close() 
