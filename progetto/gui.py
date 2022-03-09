88ed00478805c617c06ff652781df75af35a46b9
ciroooooo <100304605+ciroooooo@users.noreply.github.com>
2022-03-09 16:12:55 +0100

    gui v2

--- progetto/gui.py
+++ progetto/gui.py
@@ -17,23 +17,28 @@ def temperatura():
 def face_detection(frame):
     img, faces = detector.findFaceMesh(frame, draw=False)
     if faces:
-        print (faces)
-        print(min(faces[0,:,0]), max(faces[0,:,0]))
+        
         face = faces[0]
         pointLeft = face[145]
         pointRight = face[374]
         w, _ = detector.findDistance(pointLeft, pointRight)
-        W = 6.3
-        f = 840
+        W = 20
+        f = 230
         d = (W * f) / w
-        d=d-6
-        
+        d=d+20
+        x =max(min(np.array(faces)[0,:,0]),0)
+        y = min(max(np.array(faces)[0,:,1]),img.shape[0])
+        xn = min(max(np.array(faces)[0,:,0]),img.shape[1])
+        yn = max(min(np.array(faces)[0,:,1]),0)
         cvzone.putTextRect(img, f'Distanza: {int(d)}cm',
                            (face[10][0] - 100, face[10][1] - 50),
                            scale=2)
+        face = img.copy()[yn:y, x:xn]
+        img = cvzone.cornerRect(img,[x,yn,xn-x,y-yn], )
+                    
                                     
         #cv2.rectangle(img, bbox, (255, 0, 255), 2)
-        return True, img, img, d
+        return True, img, face, d
         
     #faccia trovata, frame con contorno della faccia, faccia, distanza
     return False, frame, frame, None 
@@ -47,8 +52,8 @@ layout = [[sg.Image(filename='', key='webcam')]]
 window = sg.Window("GUI - progetto FieraElettronica",resizable = False, grab_anywhere = True, margins = (0, 0), icon='images/logoponti.ico').Layout(layout)
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 1680)
+cap.set(4, 1120)
 pTime = 0
     
 while True:   #event loop
